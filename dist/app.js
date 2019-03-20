217a2fb574b69f95cc17afe4599c0418f6066476
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-20 17:20:58 +0300

    feat: add Array and Object Spreads

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,5 @@
 "use strict";
-var summAll = function () {
-    var array = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        array[_i] = arguments[_i];
-    }
-    return array.reduce(function (acc, item) { return acc + item; });
-};
-var summator = summAll(1, 2, 3, 4, 5, 6, 7);
-console.log(summator);
+var toppings = ['One', 'Two'];
+var otherToppings = ['Three', 'Five', 'Six'];
+var summOfToppings = toppings.concat(otherToppings);
+console.log(summOfToppings);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-const summAll = (...array:number[]) => array.reduce((acc:number, item:number) => acc + item);
+const toppings = ['One', 'Two'];
+const otherToppings = ['Three', 'Five', 'Six'];
 
-const summator = summAll(1, 2, 3, 4, 5, 6, 7);
-console.log(summator);
+const summOfToppings = [...toppings, ...otherToppings];
+console.log(summOfToppings);
