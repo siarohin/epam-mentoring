3339aa221c7bc634018527c06b4f67fc389eca3c
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-20 16:32:05 +0300

    feat: add arrow functions

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('Hello TypeScript!');
+var pizzas = [
+    {
+        name: 'Pepperoni',
+        toppings: ['pepperoni'],
+    }
+];
+var mappedPizzas = pizzas.map(function (pizza) { return pizza.name.toUpperCase(); });
+console.log(mappedPizzas);
+var pizza = {
+    name: 'Blazing Inferno',
+    getName: function () { return console.log(pizza.name); },
+};
+pizza.getName();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello TypeScript!'); 
+const pizzas = [
+    {
+        name: 'Pepperoni',
+        toppings: ['pepperoni'],
+    }
+];
+
+const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+
+const pizza = {
+    name: 'Blazing Inferno',
+    getName: () => console.log(pizza.name),
+};
+
+pizza.getName();
