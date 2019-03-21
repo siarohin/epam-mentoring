e13322d28b0e6ed4d08b375b05c96b8e618471e4
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-21 10:31:43 +0300

    feat: implement Typed Functions and Default Params

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,8 @@
 // let sumOrder: Function;
 var sumOrder;
 sumOrder = function (x, y) {
+    if (y === void 0) { y = 10; }
     return y ? x * y : x;
 };
-var sum = sumOrder(25, 10);
+var sum = sumOrder(25);
 console.log(sum);
--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,10 @@
 
 let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (x, y): number => {
+sumOrder = (x, y = 10): number => {
     return y ? x * y : x;
 }
 
-const sum = sumOrder(25, 10);
+const sum = sumOrder(25);
 
 console.log(sum);
