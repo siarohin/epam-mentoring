2c54c3691b14c9d11a3ca988da29780341a2e6c8
Aleksander Seryogin <a.siarohin@gmail.com>
2019-03-24 22:25:18 +0300

    feat: implement Event binding

--- angular-fundamentals/application/src/app/app.component.ts
+++ angular-fundamentals/application/src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title: string;
   numberOne: number = 1;
   numberTwo: number = 5;
+  name: string = 'Enter your name';
 
   // TODO
   backgroundRow: string = 'background: #333 url(https://coursehunters.net/images/logo.png) no-repeat';
@@ -24,5 +25,29 @@ export class AppComponent {
   isEven(value: number[]): boolean {
     const result = value.reduce((acc, item) => acc + item);
     return result % 2 === 0 ? true : false;
+  }
+
+  handleFocus(event: any): void {
+    if (event.target.value === this.name) {
+      event.target.value = '';
+    }
+
+    // experemental TODO ask about removeListener -- ??
+    // this event is not clone, why? remove itself??
+    this.handleBlur(event); // delete
+    this.handleClick(event); // delete
+  }
+
+  handleInput(event: any): void {
+    this.name = event.target.value;
+  }
+
+  handleBlur(event: any): void {
+    event.target.value = event.target.value || this.name;
+  }
+
+  // experemental delete
+  handleClick(event: any): void {
+    console.log('Click!');
   }
 }
