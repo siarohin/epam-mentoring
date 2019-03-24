31c469ead893f3b4c76d5122e8e5ed189e794e58
Aleksander Seryogin <a.siarohin@gmail.com>
2019-03-24 23:01:45 +0300

    feat: add sanitizer

--- angular-fundamentals/application/src/app/app.component.ts
+++ angular-fundamentals/application/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,12 @@ export class AppComponent {
   numberOne: number = 1;
   numberTwo: number = 5;
   name: string = 'Enter your name';
+  backgroundRow: object;
 
-  // TODO
-  backgroundRow: string = 'background: #333 url(https://coursehunters.net/images/logo.png) no-repeat';
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
     this.title = 'Ultimate Angular';
+    this.backgroundRow = this.sanitizer.bypassSecurityTrustStyle('background: #333 url(https://coursehunters.net/images/logo.png) no-repeat');
   }
 
   toUpperCase(str: string): string {
@@ -31,11 +32,6 @@ export class AppComponent {
     if (event.target.value === this.name) {
       event.target.value = '';
     }
-
-    // experemental TODO ask about removeListener -- ??
-    // this event is not clone, why? remove itself??
-    this.handleBlur(event); // delete
-    this.handleClick(event); // delete
   }
 
   handleInput(event: any): void {
@@ -44,10 +40,5 @@ export class AppComponent {
 
   handleBlur(event: any): void {
     event.target.value = event.target.value || this.name;
-  }
-
-  // experemental delete
-  handleClick(event: any): void {
-    console.log('Click!');
   }
 }
