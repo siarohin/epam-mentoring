bb1b5909c84042d5cce2800d8add2bcc812d8538
Aleksander Seryogin <a.siarohin@gmail.com>
2019-03-24 16:37:11 +0300

    feat: implement Arrow Functions and Lexical Scope

--- typescript-advanced-tasks/src/app.ts
+++ typescript-advanced-tasks/src/app.ts
@@ -1,29 +1,19 @@
-const myObj ={
-  name: 'Aleksandr',
-
-  doSmt() {
-    // this === myObj
+class myClass {
+  doSmth() {
+    // myClass
     console.log(this);
+
+    // window
+    setTimeout(function() {
+      console.log(this);
+    }, 0);
+
+    // myClass
+    setTimeout(() => {
+      console.log(this);
+    }, 100);
   }
-};
-
-myObj.doSmt();
-
-
-
-// <!-- Example of Call -->
-// 1. We coudn't use default value Bye:`text?: string = 'Bye' with unnecessary params === ?`
-// 2. But we could use default value`Bye: `text: string = 'Bye'` with necessary params
-function sayHi(text?: string): void {
-  console.log(`${this.name} say ${text ? text : 'Bye'}`); // if text => text, else 'Bye'
 }
-sayHi.call(myObj, 'Hi'); // Aleksandr say Hi
-sayHi.call(myObj); // Aleksandr say Bye
 
-
-
-// <!-- Example of Bind -->
-
-const myFunction = sayHi.bind(myObj, 'Hello');
-myFunction(); // Aleksandr say Hello
-
+const myInstance = new myClass();
+myInstance.doSmth();
