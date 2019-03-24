16c66ff81d42ecb80924921cedba7172d7b48e81
Aleksander Seryogin <a.siarohin@gmail.com>
2019-03-24 18:50:50 +0300

    feat: add First component with @Component

--- angular-fundamentals/application/src/app/app.component.ts
+++ angular-fundamentals/application/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'application';
+  title: string;
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
+
+  toUpperCase(str: string): string {
+    return str.toUpperCase();
+  }
 }
