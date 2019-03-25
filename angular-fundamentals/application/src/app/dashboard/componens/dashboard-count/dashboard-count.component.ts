f9226ded5cfc4012901d8a6d34b404fc5d5d87ad
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-25 15:04:59 +0300

    feat: implement Presentational (dumb) components

---
+++ angular-fundamentals/application/src/app/dashboard/componens/dashboard-count/dashboard-count.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-count',
+  templateUrl: './dashboard-count.component.html',
+  styleUrls: ['./dashboard-count.component.scss']
+})
+
+export class DashboardCountComponent {
+  constructor() {
+
+  }
+
+}
--- angular-fundamentals/application/src/app/dashboard/dashboard.module.ts
+++ angular-fundamentals/application/src/app/dashboard/dashboard.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardComponent } from './containers/dashboard/dashboard.component';
 
+import { DashboardCountComponent } from './componens/dashboard-count/dashboard-count.component';
+import { DashboardDetailComponent } from './componens/dashboard-detail/dashboard-detail.component';
+
 @NgModule ({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    DashboardCountComponent,
+    DashboardDetailComponent
   ],
   imports: [
     CommonModule,
