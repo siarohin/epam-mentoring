b9a5181e4dea614220bb0c4eab6a6c90eb52c351
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-25 13:37:12 +0300

    feat: implment Feature modules with @NgModule

--- angular-fundamentals/application/src/app/app.module.ts
+++ angular-fundamentals/application/src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DashboardModule } from './dashboard/dashboard.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    // <!-- Angular moduls -->
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // <!-- Custom moduls -->
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
